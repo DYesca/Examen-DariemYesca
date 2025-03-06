def3cee80081f144ca4f959b5cb8110ed492ca9f
DYesca <dayescaco@est.utn.ac.cr>
2025-03-05 18:31:03 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Examen-DariemYesca',
+  webDir: 'dist'
+};
+
+export default config;
